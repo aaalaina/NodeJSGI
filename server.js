7705e3d6b94038e16eee49f3fd146af94d6ce5bf
Alaina H <97alaina@gmail.com>
2022-06-22 22:58:43 -0400

    functioning code

--- server.js
+++ server.js
@@ -4,10 +4,25 @@ const data = require("./hard.json")
 //express api line
 const app = express();
 
+function getEmployeeData(index) {
+    return data[index]
+}
+
 //get request!
 app.get('/', function(req, res) {
-
-    res.send("Hello World");
+    res.send(data);
+    
+});
+app.get("/hard/:employeeID", (req, res) => {
+    const eData = getEmployeeData(req.params.employeeID);
+ if(eData){
+    res.send(eData)
+ }
+ else{
+    res.send({"error":"bad user input"});
+ }
 })
 
-app.listen(3000);
+app.listen(3000), () =>{
+    console.log("Lets see how this goes...")
+};
